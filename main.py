9bb967f21f0fd002d036d1306b130a8b81bc3302
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-15 21:34:13 +0300

    broken white display

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def has_concentrated_edges(box, edges):
 
     return concentration > 0.10
 
-def draw_boxes_on_image(img, output_path):
+def draw_boxes_on_image(img, output_path, boxes):
     draw = ImageDraw.Draw(img)
     custom_config = r'--oem 3 --psm 11'
     data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT, config=custom_config)
@@ -29,20 +29,26 @@ def draw_boxes_on_image(img, output_path):
             x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
             if has_concentrated_edges((x, y, w, h), edges) and (w * h) > 2000:
                 draw.rectangle([x-5, y-5, x + w + 5, y + h + 5], fill="black", width=1000)
+                boxes.append([x-5, y-5, x + w + 5, y + h + 5])
 
     img.save(output_path)
+    return boxes
 
 def process_and_detect(img_path, output_prefix):
     original_img = Image.open(img_path)
 
     # Convert to grayscale
     gray_img = original_img.convert('L')
-    draw_boxes_on_image(gray_img, f'{output_prefix}_gray.png')
+    # draw_boxes_on_image(gray_img, f'{output_prefix}_gray.png', [])
 
+    draw = ImageDraw.Draw(original_img)
     # Binarization using Otsu's thresholding
     threshold = 254
     binarized_img = gray_img.point(lambda p: p < threshold and 255)
-    draw_boxes_on_image(binarized_img, f'{output_prefix}_binarized.png')
+    for i in draw_boxes_on_image(binarized_img, f'{output_prefix}_binarized.png', []):
+        draw.rectangle(i, fill="white", width=1000)
+    original_img.save('a.png')
 
 img_path = 'img.jpg'
 process_and_detect(img_path, 'processed')
+
