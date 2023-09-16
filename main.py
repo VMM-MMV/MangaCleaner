e60e9260aa0085b8526300c7fcae4852bac54b8a
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-16 16:57:45 +0300

    finished rebase

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 from PIL import Image, ImageDraw
 import numpy as np
+from test import *
 
-import numpy as np
+#  with open("output.txt", "w") as f:
+#             # Convert numpy array row to string and write to file
+#             f.write(' '.join(map(str, np_image[i])))
 
-def detect_alternating_pattern(arr, min_length=5):
-    """
-    Detects an alternating pattern of 255 and 0 in the array.
-    :param arr: Input numpy array.
-    :param min_length: Minimum length of alternating pattern to be detected.
-    :return: Index where the pattern begins or None if not found.
-    """
-    
-    n = len(arr)
-    
-    for i in range(n - min_length + 1):
-        window = arr[i:i+min_length]
-        differences = np.diff(window)  # Computes difference between consecutive elements
-        if np.all(differences):  # If all differences are non-zero, it's an alternating pattern
-            return i
-    return None
+def clean_manga(img, i):
+    original_img = Image.open(img)
+    draw = ImageDraw.Draw(original_img)
 
+    gray_img = original_img.convert('L')
 
+    threshold = 254
+    binarized_img = gray_img.point(lambda p: p < threshold and 255)
 
-original_img = Image.open('img.jpg')
-draw = ImageDraw.Draw(original_img)
+    boxes = get_scattered_text_line_coordinates_into_boxes(binarized_img)
 
-gray_img = original_img.convert('L')
+    for bbox in get_text_box_that_encompases_text_line_coords(boxes):
+        min_x, min_y, max_x, max_y = bbox
+        draw.rectangle([min_x, min_y, max_x, max_y], fill="black")
 
-threshold = 254
-binarized_img = gray_img.point(lambda p: p < threshold and 255)
-
-boxes = get_scattered_text_line_coordinates_into_boxes(binarized_img)
-
-for bbox in get_text_box_that_encompases_text_line_coords(boxes):
-    min_x, min_y, max_x, max_y = bbox
-    draw.rectangle([min_x, min_y, max_x, max_y], fill="white")
+    original_img.save(f"cleaned_{i}.jpg")
 
 for i in range(1,11):
     clean_manga(r'/home/miguel/Downloads/image/1000{}.jpg'.format(i), i)
