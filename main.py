c2502ed26654d2be514ab9750e9ce813cd551d78
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-16 14:07:34 +0300

    better code readability

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def detect_alternating_pattern(arr, min_length=5):
 
 original_img = Image.open('img.jpg')
 draw = ImageDraw.Draw(original_img)
-gray_img = original_img.convert('L')
+gray_img = original_img.copy().convert('L')
 threshold = 254
 binarized_img = gray_img.point(lambda p: p < threshold and 255)
 
@@ -41,13 +41,6 @@ for i in range(np_image.shape[0]):
         coordinates = return_start_and_end_coordinates_of_bundle(np_image[i])
         if coordinates:
             for cord in coordinates:
-                
-                draw.line([(cord[0], i), (cord[1], i)], fill="black", width=5)
-        print(coordinates)
-        with open("aa.txt", "a") as f:
-                # Convert numpy array row to string and write to file
-            f.write(f'{coordinates}\n')
-    except:
-        pass
+                draw.line([(cord[0], i), (cord[1], i)], fill="black", width=115)
 
 original_img.save("a.jpg")
