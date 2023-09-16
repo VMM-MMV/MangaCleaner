e04057bc5bba7f53891094f53adec0d746e3fef7
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-16 13:13:40 +0300

    testing

--- main.py
+++ main.py
@@ -20,19 +20,27 @@ np_image = np.array(binarized_img)
 
 
 # Go through each line
-for i in range(np_image.shape[0]):
-    try:
-        # print(np_image[i])
-        coordinates = return_start_and_end_coordinates_of_bundle(np_image[i])
-        if coordinates:
-            for cord in coordinates:
+# for i in range(np_image.shape[0]):
+#     try:
+#         # print(np_image[i])
+#         coordinates = return_start_and_end_coordinates_of_bundle(np_image[i])
+#         if coordinates:
+#             for cord in coordinates:
                 
-                draw.line([(cord[0], i), (cord[1], i)], fill="black", width=5)
-        print(coordinates)
-        with open("aa.txt", "a") as f:
-                # Convert numpy array row to string and write to file
-            f.write(f'{coordinates}\n')
-    except:
-        pass
+#                 draw.line([(cord[0], i), (cord[1], i)], fill="black", width=5)
+#         print(coordinates)
+#         with open("aa.txt", "a") as f:
+#                 # Convert numpy array row to string and write to file
+#             f.write(f'{coordinates}\n')
+#     except:
+#         pass
 
+for i in range(np_image.shape[0]):
+    if i == 4040:
+        with open("output.txt", "w") as f:
+            # Convert numpy array row to string and write to file
+            f.write(' '.join(map(str, np_image[i])))
+    
+new_image = Image.fromarray(np_image)
+new_image.save("b.jpg")
 original_img.save("a.jpg")
