000962892b6e86b5e3c036259657bf5534a90ce4
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-19 11:56:09 +0300

    idk whats going on

--- binarized_cleaner.py
+++ binarized_cleaner.py
@@ -4,31 +4,40 @@ def lines(np_image, current, size):
     li = []
     for i in range(current, size):
         li.append(np_image[i])
-    return li
+    return [li, current, size]
 
-def pacman(lines, size):
+def pacman(lines):
+    current = lines[1]
+    size = lines[2]
+    lines = lines[0]
+
     li = []
     check = 0
     coords = []
     for i in range(len(lines)):
         small_coords = []
         first_column = 0
-        second_column = 0
+        last_column = 0
         for j in range(size):
-            print(lines[j][i], lines[j][i+size-1], " ", check, i, j)
-            # if sum(lines[0][:size]) == 0: 
-            #     check += 1
-            #     small_coords.append(lines[i][j])
-            #     small_coords.append(lines[i][])
-            # if sum(lines[len(lines)][:size]) == 0:
-            #     check += 1
-            # if check >= 2:
-            #     first_column += lines[j][i]
-            #     second_column += lines[j][i+size-1]
+            if sum(lines[0][:size]) == 0: 
+                check += 1
+                small_coords.append((lines[j][i], current + j))
+                small_coords.append((lines[j+size-1][i+size-1], current + j))
+            if sum(lines[len(lines)][:size]) == 0:
+                check += 1
+            if check >= 2:
+                first_column += lines[j][i]
+                last_column += lines[j][i+size-1]
+        if first_column == 0:
+            check += 1
+        if last_column == 0:
+            check += 1
+        if check == 4:
+            coords.append(small_coords)
+    print(coords)
 
 
 def clean_binarized_outliers(image):
     np_image = np.array(image)
-    pacman(lines(np_image, 0, 100), 100)
-    # for i in range(np_image.shape[0]):
-    #     print(np_image[i])
+    for i in range(np_image.shape[0]):
+        pacman(lines(np_image, i, 100))
